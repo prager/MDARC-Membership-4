5e583dc67ffd3de30d53d752669dffc2ac7a4aff
Jan Kulisek <jkulisek.us@gmail.com>
2022-02-08 20:24:10 -0800

    Activated membership report

--- app/Views/staff/staff_view.php
+++ app/Views/staff/staff_view.php
@@ -10,7 +10,7 @@
                         <h2>Welcome Staff Member!</h2>
                         <p><a href="<?php echo base_url() . '/index.php/member'; ?>" class="text-decoration-none">Your Member Page</a></p>
                         <p><a href="<?php echo base_url() . '/index.php/members-list'; ?>" class="text-decoration-none">Members Listing</a></p>
-                        <!--<p><a href="<?php //echo base_url() . '/index.php/staff-report'; ?>" class="text-decoration-none">Membership Report</a></p>-->
+                        <p><a href="<?php echo base_url() . '/index.php/staff-report'; ?>" class="text-decoration-none">Membership Report</a></p>
                         <p><a href="<?php echo base_url() . '/index.php/print-dir'; ?>" class="text-decoration-none">Print Directory View</a></p>
                         <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#tech"><i class="bi bi-chevron-right"></i> More Info </button>
                     </div>
